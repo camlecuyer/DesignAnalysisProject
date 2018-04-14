ccc64b1b161a389f159006dd1a3f4f53c7ffdf69
camlecuyer <camlecuyer@yahoo.com>
2018-04-14 08:58:00 -0500

    Zipcodes created

---
+++ zipcode generator.py
@@ -0,0 +1,12 @@
+import random
+
+data = random.sample(range(64000, 66000), 50)
+print(data)
+data.sort()
+print(data)
+
+file =  open('zipcodes.txt', 'w')
+for zcode in data:
+    file.write(str(zcode) + "\n")
+
+file.close()
